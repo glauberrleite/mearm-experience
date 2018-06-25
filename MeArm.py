e48ad483510906e308893c91f4569b60769a79e7
glauberrleite <grl@ic.ufal.br>
2018-06-25 03:38:42 +0000

    Using pigpio

--- MeArm.py
+++ MeArm.py
@@ -1,21 +1,19 @@
-import RPi.GPIO as GPIO
+import pigpio
 import numpy
 import time
 
 class MeArm:
     'An interface for GPIO operations in MeArm robotic arm'
     # Servo DutyCycle Limits based on its place at the Arm 
-    baseLimits = [4, 10]
-    leftLimits = [5, 10]
-    rightLimits = [3.5, 10]
-    handLimits = [8, 9.9]
+    baseLimits = [900, 2100]
+    leftLimits = [800, 1800]
+    rightLimits = [700, 1900]
+    handLimits = [1600, 2100]
     # Angles limits for each servo
     baseAngles = [-45, 45]
-    leftAngles = [-75, 0]
-    rightAngles = [-90, 30]
+    leftAngles = [-65, 20]
+    rightAngles = [-45, 90]
     handAngles = [0, 100]
-    # Servo Frequency
-    servoFreq = 50
 
     def __mapping(self, value,  anglesLimits, limits):
         'A function that translates a value within a range of angles to a value within the DutyCycle Limits'
@@ -32,24 +30,20 @@ class MeArm:
         return limits[0] + (scaled * (limits[1] - limits[0]))
 
     def setBase(self, value):
-        dutyCycle = self.__mapping(value, self.baseAngles, self.baseLimits)
-        self.base.ChangeDutyCycle(dutyCycle)
-        time.sleep(0.5)
+        theta1 = self.__mapping(value, self.baseAngles, self.baseLimits)
+        self.pi.set_servo_pulsewidth(self.baseServoPin, theta1)
 
     def setLeft(self, value):
-        dutyCycle = self.__mapping(value, self.leftAngles, self.leftLimits)
-        self.left.ChangeDutyCycle(dutyCycle)
-        time.sleep(0.5)
+        theta3 = self.__mapping(value, self.leftAngles, self.leftLimits)
+        self.pi.set_servo_pulsewidth(self.leftServoPin, theta3)
 
     def setRight(self, value):
-        dutyCycle = self.__mapping(value, self.rightAngles, self.rightLimits)
-        self.right.ChangeDutyCycle(dutyCycle)
-        time.sleep(0.5)
+        theta2 = self.__mapping(value, self.rightAngles, self.rightLimits)
+        self.pi.set_servo_pulsewidth(self.rightServoPin, theta2)
 
     def setHand(self, value):
-        dutyCycle = self.__mapping(value, self.handAngles, self.handLimits)
-        self.hand.ChangeDutyCycle(dutyCycle)
-        time.sleep(0.5)
+        openess = self.__mapping(value, self.handAngles, self.handLimits)
+        self.pi.set_servo_pulsewidth(self.handServoPin, openess)
 
     def setTheta(self, theta):
         self.setBase(theta[0]);
@@ -94,31 +88,25 @@ class MeArm:
 
     def closeConn(self):
         'Stops PWM and closes GPIO connection'
-        self.base.stop()
-        self.left.stop()
-        self.right.stop()
-        self.hand.stop()
-        GPIO.cleanup()
+        self.pi.stop()
     
     def __init__(self, baseServoPin, leftServoPin, rightServoPin, handServoPin, verbose=False):
-        GPIO.setmode(GPIO.BOARD)
+        
+        self.baseServoPin = baseServoPin
+        self.leftServoPin = leftServoPin
+        self.rightServoPin = rightServoPin
+        self.handServoPin = handServoPin
+
+        self.pi = pigpio.pi()
+
         # Setting pins as outputs
-        GPIO.setup(baseServoPin, GPIO.OUT)
-        GPIO.setup(leftServoPin, GPIO.OUT)
-        GPIO.setup(rightServoPin, GPIO.OUT)
-        GPIO.setup(handServoPin, GPIO.OUT)
+        self.pi.set_mode(baseServoPin, pigpio.OUTPUT)
+        self.pi.set_mode(leftServoPin, pigpio.OUTPUT)
+        self.pi.set_mode(rightServoPin, pigpio.OUTPUT)
+        self.pi.set_mode(handServoPin, pigpio.OUTPUT)
         
-        # Instantiating PWM
-        self.base = GPIO.PWM(baseServoPin, self.servoFreq)
-        self.left = GPIO.PWM(leftServoPin, self.servoFreq)
-        self.right = GPIO.PWM(rightServoPin, self.servoFreq)
-        self.hand = GPIO.PWM(handServoPin, self.servoFreq)
-        
-        # Starting PWM and defining initial Duty Cycle
-        self.base.start(7)
-        self.left.start(8)
-        self.right.start(4.7)
-        self.hand.start(9.9)
+        self.setTheta([0, 0, 0])
+        self.setHand(0)
 
         self.verbose = verbose
 
--- mapping.py
+++
@@ -1,7 +0,0 @@
-def mapping(value,  anglesLimits, limits):
-        'A function that translates a value within a range of angles to a value within the DutyCycle Limits'
-        span = anglesLimits[1] - anglesLimits[0]
-        scaled = float(value - anglesLimits[0])/float(span)
-        return limits[0] + (scaled * (limits[1] - limits[0]))
-
-print mapping(-30, [-75, 0], [5, 10])
