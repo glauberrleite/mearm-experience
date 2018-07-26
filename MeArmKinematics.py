cde04a32929c082f8aa93f8cb8cb2368c13661b0
glauberrleite <grl@ic.ufal.br>
2018-07-26 08:16:15 -0300

    python2 compat

--- MeArmKinematics.py
+++ MeArmKinematics.py
@@ -56,56 +56,57 @@ class MeArmKinematics:
         return x_e
 
     def jacobian(self, theta):
-        [x_1, x_2, x_3, x_4] = self.dh2dq(theta);
+        [x_1, x_2, x_3, x_4] = self.dh2dq(theta)
 
-        q = np.zeros(8);
+        q = np.zeros(8)
 
-        q[0] = -(1/2)*np.sin(theta[0]/2)*np.cos(pi/4);
-        q[1] = -(1/2)*np.sin(theta[0]/2)*np.sin(pi/4);
-        q[2] = (1/2)*np.cos(theta[0]/2)*np.sin(pi/4);
-        q[3] = (1/2)*np.cos(theta[0]/2)*np.cos(pi/4);
+        q[0] = -(1.0/2)*np.sin(theta[0]/2)*np.cos(pi/4)
+        q[1] = -(1.0/2)*np.sin(theta[0]/2)*np.sin(pi/4)
+        q[2] = (1.0/2)*np.cos(theta[0]/2)*np.sin(pi/4)
+        q[3] = (1.0/2)*np.cos(theta[0]/2)*np.cos(pi/4)
 
-        dx_1_dtheta_1 = DQ(q);
+        dx_1_dtheta_1 = DQ(q)
 
-        q = np.zeros(8);
+        q = np.zeros(8)
 
-        q[0] = -(1/2)*np.sin((theta[1] + pi/2)/2);
-        q[3] = (1/2)*np.cos((theta[1] + pi/2)/2);
-        q[5] = (self.L_1/2) * q[0];
-        q[6] = (self.L_2/2) * q[3];
+        q[0] = -(1.0/2)*np.sin((theta[1] + pi/2)/2)
+        q[3] = (1.0/2)*np.cos((theta[1] + pi/2)/2)
+        q[5] = (self.L_1/2) * q[0]
+        q[6] = (self.L_2/2) * q[3]
 
-        dx_2_dtheta_2 = DQ(q);
+        dx_2_dtheta_2 = DQ(q)
 
-        q = np.zeros(8);
+        q = np.zeros(8)
 
-        q[0] = (1/2)*np.sin((theta[2] - theta[1] - pi/2)/2);
-        q[3] = -(1/2)*np.cos((theta[2] - theta[1] + pi/2)/2);
-        q[5] = (self.L_1/2) * q[0];
-        q[6] = (self.L_2/2) * q[3];
+        q[0] = (1.0/2)*np.sin((theta[2] - theta[1] - pi/2)/2)
+        q[3] = -(1.0/2)*np.cos((theta[2] - theta[1] + pi/2)/2)
+        q[5] = (self.L_1/2) * q[0]
+        q[6] = (self.L_2/2) * q[3]
 
-        dx_3_dtheta_2 = DQ(q);
+        dx_3_dtheta_2 = DQ(q)
 
-        q = np.zeros(8);
+        q = np.zeros(8)
 
-        q[0] = -(1/2)*np.sin((theta[2] - theta[1] - pi/2)/2);
-        q[3] = (1/2)*np.cos((theta[2] - theta[1] + pi/2)/2);
-        q[5] = (self.L_1/2) * q[0];
-        q[6] = (self.L_2/2) * q[3];
+        q[0] = -(1.0/2)*np.sin((theta[2] - theta[1] - pi/2)/2)
+        q[3] = (1.0/2)*np.cos((theta[2] - theta[1] + pi/2)/2)
+        q[5] = (self.L_1/2) * q[0]
+        q[6] = (self.L_2/2) * q[3]
 
-        dx_3_dtheta_3 = DQ(q);
+        dx_3_dtheta_3 = DQ(q)
 
-        q = np.zeros(8);
+        q = np.zeros(8)
 
-        q[0] = -(1/2)*np.sin(theta[2]/2)*np.cos(pi/4);
-        q[1] = (1/2)*np.sin(theta[2]/2)*np.sin(pi/4);
-        q[2] = (1/2)*np.cos(theta[2]/2)*np.sin(pi/4);
-        q[3] = -(1/2)*np.cos(theta[2]/2)*np.cos(pi/4);
-
-        dx_4_dtheta_3 = DQ(q);
-
+        q[0] = -(1.0/2)*np.sin(theta[2]/2)*np.cos(pi/4)
+        q[1] = (1.0/2)*np.sin(theta[2]/2)*np.sin(pi/4)
+        q[2] = (1.0/2)*np.cos(theta[2]/2)*np.sin(pi/4)
+        q[3] = -(1.0/2)*np.cos(theta[2]/2)*np.cos(pi/4)
+        
+        dx_4_dtheta_3 = DQ(q)
+                
         J = np.array([vec8(dx_1_dtheta_1*x_2*x_3*x_4), \
                 vec8(x_1*dx_2_dtheta_2*x_3*x_4 + x_1*x_2*dx_3_dtheta_2*x_4), \
                 vec8(x_1*x_2*dx_3_dtheta_3*x_4 + x_1*x_2*x_3*dx_4_dtheta_3)])
+
         J = np.transpose(J)
 
         return J
--- position_control.py
+++ position_control.py
@@ -1,5 +1,5 @@
 from MeArmKinematics import MeArmKinematics
-from MeArm import MeArm
+#from MeArm import MeArm
 from DQ import *
 import numpy as np
 import time
@@ -12,7 +12,7 @@ else:
     position = np.array([float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3])])
 
 kinematics = MeArmKinematics()
-mearm = MeArm(baseServoPin = 4, rightServoPin = 17, leftServoPin = 27, handServoPin = 22)
+#mearm = MeArm(baseServoPin = 4, rightServoPin = 17, leftServoPin = 27, handServoPin = 22)
 
 theta = np.array([0, 0, 0])
 
@@ -31,13 +31,14 @@ while np.linalg.norm(error) > epsilon:
     x = kinematics.fkm(theta)
     J = kinematics.jacobian(theta)
     error = vec8(xd - x)
-    theta = theta + np.linalg.pinv(J) @ error * dt
 
-    mearm.setTheta(theta * 180/np.pi)
+    theta = theta + np.dot(np.linalg.pinv(J), error) * dt
+
+#    mearm.setTheta(theta * 180/np.pi)
 
     time.sleep(0.0001)
     print(theta * 180 / np.pi)
 
-mearm.setHand(95)
-input()
-mearm.closeConn()
+#mearm.setHand(95)
+raw_input()
+#mearm.closeConn()
